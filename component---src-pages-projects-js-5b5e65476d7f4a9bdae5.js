4912bb195563364390f2040299104bfe03ceda9a
dmcc <dmccrthy@users.noreply.github.com>
2023-12-04 14:24:20 -0500

    Updates

---
+++ component---src-pages-projects-js-5b5e65476d7f4a9bdae5.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkdmccrthy_github_io=self.webpackChunkdmccrthy_github_io||[]).push([[853],{7219:function(e,t,n){n.r(t),n.d(t,{Head:function(){return m}});var l=n(7294),c=n(5086),r=n(9153),a=n(2777),i=n(4271),u=n(6056);const h=c.ZP.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 84vh;
+`;t.default=()=>l.createElement("main",null,l.createElement(r.Z,null),l.createElement(i.Z,null),l.createElement(h,null,l.createElement("h1",null,"I'm Working on it...")),l.createElement(u.Z,null));const m=()=>l.createElement(a.Z,{title:"Projects | Dan McCarthy"})}}]);
+//# sourceMappingURL=component---src-pages-projects-js-5b5e65476d7f4a9bdae5.js.map
